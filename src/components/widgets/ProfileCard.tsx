c3067343c3391570bcba73a4a8a84f3637ba839d
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-07-26 08:49:21 +0700

    scaffold: profile card & home layout

---
+++ src/components/widgets/ProfileCard.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+export default function ProfileCard(): ReactNode {
+    return (
+        <h1>
+            Card
+        </h1>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import type { ReactNode } from "react";
 
+import ProfileCard from "@/components/widgets/ProfileCard";
+
 export default function Home(): ReactNode {
   return (
-    <h1>Home</h1>
+    <div className='flex sm:flex-col md:flex-row gap-4 justify-center items-center text-white'>
+      <div>
+        <ProfileCard />
+      </div>
+      <div>
+        Post list
+      </div>
+    </div>
   )
 }
