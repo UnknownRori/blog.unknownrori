7ab8ed1b9fd691fd8ba45af7bfdfcf74451467f1
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-08-06 17:28:38 +0700

    add: more method

--- src/interface/repository/PostRepository.ts
+++ src/interface/repository/PostRepository.ts
@@ -2,5 +2,7 @@ import type { Post } from "@/models/Posts";
 
 export default interface PostRepository {
     all(): Array<Post>;
-    find(id: string): Array<Post>;
+    insert(post: Post): void;
+    delete(id: string): void;
+    find(id: string): Post | undefined;
 }
--- src/interface/repository/SeriesRepository.ts
+++ src/interface/repository/SeriesRepository.ts
@@ -2,5 +2,5 @@ import type { Series } from "@/models/Series";
 
 export default interface SeriesRepository {
     all(): Array<Series>;
-    find(id: string): Array<Series>;
+    find(id: string): Series | undefined;
 }
