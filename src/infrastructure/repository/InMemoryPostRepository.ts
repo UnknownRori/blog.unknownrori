d325fe7585b302f34c65e874716b68cb6732a28a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-08-06 17:29:42 +0700

    add: implement InMemoryPostRepository

---
+++ src/infrastructure/repository/InMemoryPostRepository.ts
@@ -0,0 +1,27 @@
+import type PostRepository from "@/interface/repository/PostRepository";
+import type { Post } from "@/models/Posts";
+
+export default class InMemoryPostRepository implements PostRepository {
+    private data: Array<Post>;
+
+    constructor(data: Array<Post> = []) {
+        this.data = data;
+    }
+
+    all(): Array<Post> {
+        return this.data;
+    }
+
+    insert(post: Post): void {
+        this.data.push(post);
+    }
+
+    delete(id: string): void {
+        this.data = this.data.filter((post) => post.id !== id);
+    }
+
+    find(id: string): Post | undefined {
+        return this.data.find((post) => post.id === id);
+    }
+
+}
